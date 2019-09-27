949e8fd97e897932821d4014145ecfa2a78596a9
Veraprot <vera.protopopova@gmail.com>
2019-09-27 10:32:34 -0400

    links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import linkedin from '../../src/assets/images/linkedin.svg'
 import github from '../../src/assets/images/github.svg'
+import message from '../../src/assets/images/message.svg'
 
-const Navbar = (props) => {
+const Navbar = () => {
   return (
     <div className="nav-links fixed-menu">
       <ul className="nav-icons">
         <li>
           <img src={linkedin} alt=""/>
         </li>
-        <li><img src={github} alt=""/></li>
+        <li>
+          <img src={github} alt=""/>
+        </li>
+        <li>
+          <img src={message} alt=""/>
+        </li>
       </ul>
       <ul className="links">
         <li>
@@ -19,7 +25,7 @@ const Navbar = (props) => {
           <a href="#experience">Experience</a>
         </li>
         <li>
-          <a href="">Projects</a>
+          <a href="#projects">Projects</a>
         </li>
       </ul>
     </div>

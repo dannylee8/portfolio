5c0541b7b7e2f20f6256dd9be35629d9ee677aca
Danny Lee <dreamer@panix.com>
2019-09-29 22:39:19 -0400

    typo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
     <div className="nav-links fixed-menu">
       <ul className="nav-icons">
         <li>
-          <a target="_blank" href="hhttps://www.linkedin.com/in/vera-protopopova"><img src={linkedin} alt="Vera@LinkedIn"/></a>
+          <a target="_blank" href="https://www.linkedin.com/in/vera-protopopova"><img src={linkedin} alt="Vera@LinkedIn"/></a>
         </li>
         <li>
           <a target="_blank" href="https://github.com/Veraprot"><img src={github} alt="Vera@Github"/></a>
